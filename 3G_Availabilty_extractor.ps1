45bd61492ece55394a97f535d17b3c93aada13b7
sinasaeednia <55633409+sinasaeednia@users.noreply.github.com>
2023-06-08 19:41:11 -0400

    Update 3G_Availabilty_extractor.ps1

--- 3G_Availabilty_extractor.ps1
+++ 3G_Availabilty_extractor.ps1
@@ -1,5 +1,7 @@
-﻿$filename=".\downsite.log"
+﻿$filename="downsite.log"
 $search="Object"
+$output_file="site_list.txt"
+
 
 #gets the line number of string $search
 $linenumber= Get-Content $filename | select-string $search
@@ -14,4 +16,25 @@ $temp_output = Get-Content $filename | Select-Object -Skip ($linenumber.LineNumb
 $bottom_line = ($temp_output | select-string "Log close:").LineNumber - 3
 
 # reads N number of file and write it to output
-$temp_output | select -First $bottom_line | Out-File sites_list.txt
+$temp_output = $temp_output | select -First $bottom_line # | Out-File $output_file
+
+# Counting number of hours in output (to compare withnumber of 100
+$count_of_hours = (($temp_output | Select -First 1).Split(':')).count-1 
+
+
+# compares number of 100 (using replace) with $count_of_hours and remove them          $count_of_100 = (Get-Content $output_file | Select-Object -Skip 1 | Select -First 1).replace('100','@').Split('@').count-1 
+ $result = switch ($temp_output) {
+        { $_.replace('100','@').Split('@').count-1  -ne $count_of_hours } { $_ }  # only output lines that are less or equal to 4000 characters
+        # default { $skipped++ }         # count the lines that were longer than 4000 characters
+    }
+
+$result | Out-File $output_file
+
+
+<#  Release notes
+
+V01:
+    Filters lines with all availabilities set to 100 
+V00:
+    Extracts table like information from .log output file
+#>
