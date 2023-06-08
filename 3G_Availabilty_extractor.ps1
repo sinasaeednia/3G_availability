4dc552be9eb5807a3f034bea110a937d04951e25
sinasaeednia <55633409+sinasaeednia@users.noreply.github.com>
2023-06-08 19:40:45 -0400

    Create 3G_Availabilty_extractor.ps1

---
+++ 3G_Availabilty_extractor.ps1
@@ -0,0 +1,17 @@
+﻿$filename=".\downsite.log"
+$search="Object"
+
+#gets the line number of string $search
+$linenumber= Get-Content $filename | select-string $search
+$linenumber.LineNumber
+
+#removes all lines before line number -1 (to include headers)
+$temp_output = Get-Content $filename | Select-Object -Skip ($linenumber.LineNumber-1) #| Out-File sites_list.log
+
+
+
+#gets the line number of string "Log close:" to clear it from bottom of output
+$bottom_line = ($temp_output | select-string "Log close:").LineNumber - 3
+
+# reads N number of file and write it to output
+$temp_output | select -First $bottom_line | Out-File sites_list.txt
